2b5d22c99cbc968b2d72049c30f248bdb7828bea
tsi4456 <cremedefraise@gmail.com>
2025-06-04 20:21:07 +0100

    player code

--- main.py
+++ main.py
@@ -2,13 +2,32 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+        pygame.display.flip()
+        dt = timer.tick(60) / 1000
 
 
 if __name__ == "__main__":
